7ffa5cd80b308fcefd03090fa197f1fba1b3e177
MarynaKarpenko <mariina.karpenko@gmail.com>
2024-02-29 14:04:00 +0100

    change

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -3,13 +3,56 @@ import "./CSS/LoginSignUp.css";
 
 const LoginSignUp = () => {
   const [state, setState] = useState("Login");
+  const [formData, setFormData] = useState({
+    userName: "",
+    password: "",
+    email: "",
+  });
+
+  const changeHandler = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   const login = async () => {
-    console.log("Login Function Executed");
+    console.log("Login Function Executed", formData);
+    let responseData;
+    await fetch("http://localhost:4000/login", {
+      method: "POST",
+      headers: {
+        Accept: "application/form-data",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => (responseData = data));
+    if (responseData.success) {
+      localStorage.setItem("auth-token", responseData.token);
+      window.location.replace("/");
+    } else {
+      alert(responseData.errors);
+    }
   };
 
   const signUp = async () => {
-    console.log("Sign Up Function Executed");
+    console.log("Sign Up Function Executed", formData);
+    let responseData;
+    await fetch("http://localhost:4000/signup", {
+      method: "POST",
+      headers: {
+        Accept: "application/form-data",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => (responseData = data));
+    if (responseData.success) {
+      localStorage.setItem("auth-token", responseData.token);
+      window.location.replace("/");
+    } else {
+      alert(responseData.errors);
+    }
   };
 
   return (
@@ -18,12 +61,30 @@ const LoginSignUp = () => {
         <h1>{state}</h1>
         <div className="login-sign-up-fields">
           {state === "Sign Up" ? (
-            <input type="text" placeholder="Your Name" />
+            <input
+              name="userName"
+              value={formData.userName}
+              onChange={changeHandler}
+              type="text"
+              placeholder="Your Name"
+            />
           ) : (
             <></>
           )}
-          <input type="email" placeholder="Email Address" />
-          <input type="password" placeholder="Password" />
+          <input
+            name="email"
+            value={formData.email}
+            onChange={changeHandler}
+            type="email"
+            placeholder="Email Address"
+          />
+          <input
+            name="password"
+            value={formData.password}
+            onChange={changeHandler}
+            type="password"
+            placeholder="Password"
+          />
         </div>
         <button
           onClick={() => {
